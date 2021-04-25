311f372889c24700937ba16e2f7070e5da04cde8
davidka7 <david.perekopskiy@go.shoreline.edu>
2021-04-25 16:01:34 -0700

    s

--- src/components/List.js
+++ src/components/List.js
@@ -15,28 +15,51 @@ function List() {
           <div className="back">LOVE IT</div>
         </div>
       </div>
-      <br /> <br /> <br /> <br /> <br /> <br /> <br />
-      <div className="flex">
-        <div id="awesome">your awesome content</div>
+      <div className="flex">About</div>{" "}
+      <div className="flexx">
+        <div id="awesome">
+          <br />
+          While in school, I spent time building my coding abilities, starting
+          from ruby on rails and working on React javascript, I came to be able
+          to adapt and grasp any language I put my mind to. I have worked on my
+          own time and at my current internship, with redux, hooks, typescript,
+          Mongo DB, writing JSON files, stripe, and other tools. I am focused
+          and diligent when managing workloads and prioritizing tasks to meet
+          deadlines. I thrive in environments where I am able to make a direct
+          impact, utilizing my abilities in complex problem-solving to find
+          solutions and achieve results. I also love to learn what I don't know.
+          As for my background, I grew up with a single mom, a strong woman, who
+          raised me well, but we had no English language(we are Russian), so it
+          was quite difficult for us at first. But I was fast to grasp the
+          English language and learned a lot. I loved building since childhood
+          and that's partially the reason I got into coding. I'm not sure about
+          the program I used but from day one when I was able to write some code
+          and see instant results, the step by step process of my code on the
+          console, I grew a passion for coding. My most valuable skills are my
+          desire to learn and adapt to new environments and my knowledge of the
+          Russian and Ukrainian language(if ever needed). I have a grasp in
+          different technologies and more is said about it and my internship on
+          my resume. <br />
+          Sincerely, david perekopskiy
+        </div>
+        <br />
       </div>
-      <br /> <br /> <br /> <br /> <br /> <br />
       <div className="flex">
         <div id="hobbies">Hobbies</div>
       </div>{" "}
-      <br /> <br /> <br /> <br /> <br /> <br />
+      <div className="flexx">lol</div>
       <div className="flex">
         <div id="projects">Projects</div>
       </div>
-      <br /> <br /> <br /> <br /> <br /> <br />
+      <div className="flexx">lol</div>
       <div className="flex">
         <div id="experience">experience</div>
       </div>{" "}
-      <br /> <br /> <br /> <br />
+      <div className="flexx">lol</div>
       <div className="flex">
         <div id="contact">contact</div>
       </div>{" "}
-      <br /> <br /> <br /> <br />
-      <br /> <br /> <br /> <br />
+      <div className="flexx">lol</div>
     </div>
   );
 }
